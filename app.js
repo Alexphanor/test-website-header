8beb89d072dbf56cb782ae4593f566c2c02d4396
Alexphanor <alexandrephanor@gmail.com>
2018-05-05 18:10:04 -0400

    test responsive header

---
+++ app.js
@@ -0,0 +1,11 @@
+
+
+function openSideMenu() {
+  document.querySelector("#side-menu").style.width = "250px";
+  document.querySelector(".row").style.marginLeft = "250px";
+}
+
+function closeSideMenu() {
+  document.querySelector("#side-menu").style.width = "0px";
+  document.querySelector(".row").style.marginLeft = "-15px";
+}
